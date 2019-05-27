4e1b9b9deebb35ca1fbef3fbc780fc3640aae9f0
Thinker <mstr.raphael@gmail.com>
2019-05-27 18:12:14 +0800

    improve the look and feel of iframe widgets

--- source/js/iframe-widget.js
+++ source/js/iframe-widget.js
@@ -2,17 +2,18 @@
     var iFrames = $('iframe');
     function iResize() {
       for (var i = 0, j = iFrames.length; i < j; i++) {
-        var frame = iFrames[i]
-        var body = frame.contentWindow.document.body
+        var frame = iFrames[i];
+        var body = frame.contentWindow.document.body;
         // iFrames[i].style.height = iFrames[i].contentWindow.document.body.offsetHeight + 'px';
         // iFrames[i].contentWindow.document.body.style.width = iFrames[i].offsetWidth + 'px';
-        $(body).width($(frame).width())
-        $(frame).height($(body).height())
+        $(body).width($(frame).width());
+        $(frame).height($(body).height());
       }
     }
 
     iFrames.on("load", function(){
       setTimeout(iResize, 0);
+      $(window).on("resize", iResize);
     });
 
     // if ($.browser.webkit || $.browser.opera) {
